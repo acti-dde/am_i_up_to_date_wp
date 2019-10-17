31f38c8b9c51551e696dc8c2c877b752ffd3bd03
a.delcroix <a.delcroix@acti.fr>
2019-10-17 16:17:40 +0200

    Add status code and json type

--- src/Controller/RouteController.php
+++ src/Controller/RouteController.php
@@ -1,47 +1,73 @@
 <?php
 
-final class RouteController
-{
-    private $_routeUrl;
-
-    private $_apiKey;
-
-    private $_headerApiKey;
-
-    public function __construct()
+    final class RouteController
     {
-        $this->_routeUrl = get_option('aiutd_route_url');
-        $this->_apiKey = get_option('aiutd_api_key');
-        $this->_headerApiKey = 'api_key';
+        private $_routeUrl;
 
-        add_action('template_redirect', array($this, 'sendData'));
-    }
+        private $_apiKey;
 
-    public function sendData()
-    {
-        global $wp;
+        private $_headerApiKey;
 
-        if ($this->_routeUrl && $this->_apiKey && $wp->request == $this->_routeUrl) {
-            $headers = getallheaders();
-            if (isset($headers[$this->_headerApiKey]) && $headers[$this->_headerApiKey] == $this->_apiKey) {
-                $data = $this->_buildData();
-                echo json_encode($data);
-                exit;
-            } else {
-                wp_redirect(home_url());
+        public function __construct()
+        {
+            $this->_routeUrl = get_option('aiutd_route_url');
+            $this->_apiKey = get_option('aiutd_api_key');
+            $this->_headerApiKey = 'Apikey';
+
+            add_action('parse_request', array($this, 'sendData'), 1);
+        }
+
+        public function sendData()
+        {
+            global $wp;
+
+            if ($this->_routeUrl && $this->_apiKey && $wp->request == $this->_routeUrl) {
+                nocache_headers();
+                header('Content-Type: application/json');
+
+                $requestMethod = $_SERVER['REQUEST_METHOD'];
+                $headers = $this->_getHeaders();
+                if (isset($headers[$this->_headerApiKey]) && $requestMethod == 'GET') {
+                    if ($headers[$this->_headerApiKey] == $this->_apiKey) {
+                        status_header(200);
+                        $data = $this->_buildData();
+                        $result = json_encode($data);
+                    } else {
+                        status_header(401);
+                        $result = json_encode([
+                            'message' => 'Token invalide'
+                        ]);
+                    }
+                } else {
+                    status_header(404);
+                    $result = json_encode([
+                        'message' => '404 route non trouvée',
+                    ]);
+                }
+                echo $result;
                 exit;
             }
         }
-    }
 
-    private function _buildData()
-    {
-        $data = array();
-        $dataController = new DataController();
-        $data['php'] = $dataController->getPhpVersion();
-        $data['cms'] = 'wordpress-' . $dataController->getWordpressVersion();
-        $data['plugins'] = $dataController->getPluginsVersions();
+        private function _buildData()
+        {
+            $data = array();
+            $dataController = new DataController();
+            $data['php'] = (explode('-', phpversion()))[0];
+            $data['cms'] = 'wordpress-' . $dataController->getWordpressVersion();
+            $data['plugins'] = $dataController->getPluginsVersions();
 
-        return $data;
+            return $data;
+        }
+
+        private function _getHeaders()
+        {
+            $headers = [];
+            foreach ($_SERVER as $name => $value) {
+                if (substr($name, 0, 5) == 'HTTP_') {
+                    $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                }
+            }
+            return $headers;
+        }
     }
-}
